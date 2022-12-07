6562814d971d06fe2d9ee0f4b2c3d4427b2f4e89
angrms <119953245+angrms@users.noreply.github.com>
2022-12-07 20:12:18 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -389,7 +389,7 @@ function removeRankedTrainee(trainee) {
   return false;
 }
 
-const currentURL = "https://produce48.github.io/";
+const currentURL = "https://angrms.github.io/DreamMaker/";
 // Serializes the ranking into a string and appends that to the current URL
 function generateShareLink() {
   let shareCode = ranking.map(function (trainee) {
